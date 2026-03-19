4e2ee2c585f2dccf35e0dd9efcdfc9ca07d6e6c7
jacob-leeming-netcompany <235530878+jacob-leeming-netcompany@users.noreply.github.com>
2026-03-19 13:03:43 +0000

    contact details get & put unit tests

---
+++ test/uk/gov/hmrc/senioraccountingofficer/controllers/ContactDetailsControllerSpec.scala
@@ -0,0 +1,161 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.senioraccountingofficer.controllers
+
+import org.mockito.Mockito.{reset, when}
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatestplus.mockito.MockitoSugar.mock
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.Application
+import play.api.http.Status
+import play.api.inject.bind
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.test.FakeRequest
+import play.api.test.Helpers.*
+import uk.gov.hmrc.http.HttpResponse
+import uk.gov.hmrc.senioraccountingofficer.connectors.ContactDetailsConnector
+import org.mockito.ArgumentMatchers.{any, eq as meq}
+import scala.concurrent.Future
+
+class ContactDetailsControllerSpec extends AnyWordSpec with Matchers with GuiceOneAppPerSuite {
+
+  private val mockContactDetailsConnector = mock[ContactDetailsConnector]
+
+  override def fakeApplication(): Application =
+    GuiceApplicationBuilder()
+      .overrides(
+        bind[ContactDetailsConnector].toInstance(mockContactDetailsConnector)
+      )
+      .build()
+
+  private val validPayload =
+    """{
+      |  "safeId": "XE000123456789",
+      |  "company": {
+      |    "companyName": "Acme Manufacturing Ltd",
+      |    "uniqueTaxReference": "1234567890",
+      |    "companyRegistrationNumber": "OC123456"
+      |  },
+      |  "contacts": [
+      |    {
+      |      "name": "Jane Doe",
+      |      "email": "jane.doe@example.com"
+      |    }
+      |  ]
+      |}""".stripMargin
+
+  "PUT /contact-details" should {
+    "return 204 when the downstream connector succeeds without a body" in {
+      reset(mockContactDetailsConnector)
+      when(mockContactDetailsConnector.putContactDetails(any(), any())(using any()))
+        .thenReturn(Future.successful(HttpResponse(status = Status.NO_CONTENT)))
+
+      val request = FakeRequest("PUT", "/senior-accounting-officer/contact-details/123")
+        .withHeaders(CONTENT_TYPE -> "application/json")
+        .withTextBody(validPayload)
+
+      val maybeResult = route(app, request)
+
+      maybeResult shouldBe defined
+      val result = maybeResult match {
+        case Some(value) => value
+        case None        => fail("Expected route to be defined")
+      }
+
+      status(result) shouldBe Status.NO_CONTENT
+    }
+
+    "return the downstream JSON body for validation errors" in {
+      val downstreamBody = """[{"path":"safeId","reason":"INVALID_FORMAT"}]"""
+      reset(mockContactDetailsConnector)
+      when(mockContactDetailsConnector.putContactDetails(any(), any())(using any()))
+        .thenReturn(Future.successful(HttpResponse(status = Status.BAD_REQUEST, body = downstreamBody)))
+
+      val request = FakeRequest("PUT", "/senior-accounting-officer/contact-details/123")
+        .withHeaders(CONTENT_TYPE -> "application/json")
+        .withTextBody(validPayload)
+
+      val maybeResult = route(app, request)
+
+      maybeResult shouldBe defined
+      val result = maybeResult match {
+        case Some(value) => value
+        case None        => fail("Expected route to be defined")
+      }
+
+      status(result) shouldBe Status.BAD_REQUEST
+      contentAsString(result) shouldBe downstreamBody
+    }
+  }
+
+  "GET /contact-details" should {
+    "return a 200 response with the body from the connector" in {
+      val saoSubscriptionId = "123"
+
+      val expectedStatus = 200
+      val expectedBody   = "{}"
+
+      when(mockContactDetailsConnector.getContactDetails(meq(saoSubscriptionId))(using any())) thenReturn Future
+        .successful(
+          HttpResponse(expectedStatus, expectedBody)
+        )
+
+      val url = routes.ContactDetailsController.getContactDetails(saoSubscriptionId).url
+
+      val request = FakeRequest(GET, url)
+
+      val maybeResult = route(app, request)
+
+      maybeResult shouldBe defined
+      val result = maybeResult match {
+        case Some(value) => value
+        case None        => fail("Expected route to be defined")
+      }
+
+      status(result) shouldBe expectedStatus
+      contentAsString(result) shouldBe expectedBody
+    }
+
+    "return a 404 response with the body from the connector" in {
+      val invalidSaoSubscriptionId = "456"
+
+      val expectedStatus = 404
+      val expectedBody   = "Entity not found"
+
+      when(mockContactDetailsConnector.getContactDetails(meq(invalidSaoSubscriptionId))(using any())) thenReturn Future
+        .successful(
+          HttpResponse(expectedStatus, expectedBody)
+        )
+
+      val url = routes.ContactDetailsController.getContactDetails(invalidSaoSubscriptionId).url
+
+      val request = FakeRequest(GET, url)
+
+      val maybeResult = route(app, request)
+
+      maybeResult shouldBe defined
+      val result = maybeResult match {
+        case Some(value) => value
+        case None        => fail("Expected route to be defined")
+      }
+
+      status(result) shouldBe expectedStatus
+      contentAsString(result) shouldBe expectedBody
+    }
+  }
+}
