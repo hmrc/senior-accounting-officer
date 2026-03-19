2770440d720d08727ce84ae25e14543ebb9bc483
jacob-leeming-netcompany <235530878+jacob-leeming-netcompany@users.noreply.github.com>
2026-03-19 09:43:38 +0000

    contact details get implementation

---
+++ app/uk/gov/hmrc/senioraccountingofficer/controllers/ContactDetailsController.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.senioraccountingofficer.controllers
+
+import play.api.mvc.ControllerComponents
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+import scala.concurrent.ExecutionContext
+import javax.inject.Inject
+import play.api.mvc.Action
+import uk.gov.hmrc.play.http.HeaderCarrierConverter
+import uk.gov.hmrc.senioraccountingofficer.connectors.ContactDetailsConnector
+import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse}
+
+class ContactDetailsController @Inject() (cc: ControllerComponents, stubConnector: ContactDetailsConnector)(using
+    ExecutionContext
+) extends BackendController(cc) {
+  def getContactDetails(saoSubscriptionId: String): Action[String] = Action.async(parse.tolerantText) {
+    implicit request =>
+      given HeaderCarrier = HeaderCarrierConverter.fromRequest(request)
+      stubConnector.getContactDetails(saoSubscriptionId).map { case HttpResponse(status, body, _) =>
+        Status(status)(body)
+      }
+  }
+
+  def putContactDetails(saoSubscriptionId: String): Action[String] = {
+    ???
+  }
+}
